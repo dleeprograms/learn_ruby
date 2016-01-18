4d9771c45f42d4a37f0945a4e6454df8f26d2b5f
David Dongseung Lee <dleeprograms@outlook.com>
2016-01-18 17:35:04 -0500

    exercise1_3

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,27 @@
+
+def add(x, y)
+
+  x + y
+
+end
+
+
+
+def subtract(x, y)
+
+  x - y
+
+end
+
+
+def sum(n)
+
+  (n).inject { |sum, n| sum + n }
+
+end
+
+# def sum(7)
+# 
+#   (7).inject { |sum, n| sum + 7 }
+#
+# end
